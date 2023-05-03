9938b30ee80a70319a08469f271a717af29e246c
shubhali <shubhalishah57@gmail.com>
2023-05-04 00:24:30 +0530

    routing

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import { Route,Routes } from 'react-router-dom';
+import SignUp from './Pages/Signup/SignUp';
+import LogIn from './Pages/Login/LogIn';
+
 
 function App() {
   return (
     <div className="App">
-   
+      <Routes>     
+        
+          <Route path ='/' element={<SignUp/>}/>
+          <Route path ='/login' element={<LogIn/>}/>
+
+      </Routes>
+ 
+
+
    
     </div>
   );
